62e4c019785d6a2e96e50f0ee0a0b815f1cd0d11
Blackeye-101 <rishabh.th25@gmail.com>
2022-07-03 13:13:07 +0530

    navbar and small-sidebar frontend setup

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,40 @@
 import Wrapper from "../assets/wrappers/Navbar";
-import { FaHome } from "react-icons/fa";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
+import Logo from "./Logo";
+import { useState } from "react";
+
 const Navbar = () => {
+  const { user, toggleSidebar, logoutUser } = useAppContext();
+  const [showLogout, setShowLogout] = useState(false);
   return (
     <Wrapper>
-      <h4>Navbar</h4>
-      <FaHome></FaHome>
+      <div className="nav-center">
+        <button type="button" className="toggle-btn" onClick={toggleSidebar}>
+          <FaAlignLeft></FaAlignLeft>
+        </button>
+        <div>
+          <Logo></Logo>
+          <h3 className="logo-text">Dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => setShowLogout(!showLogout)}
+          >
+            <FaUserCircle></FaUserCircle>
+            {user?.name}
+            {/* optional chaining */}
+            <FaCaretDown></FaCaretDown>
+          </button>
+          <div className={showLogout ? "dropdown show-dropdown" : "dropdown"}>
+            <button type="button" className="dropdown-btn" onClick={logoutUser}>
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
