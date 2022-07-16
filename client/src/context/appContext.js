6dc46fe855ce999929e31b0ebbc916dfa40dc174
Blackeye-101 <rishabh.th25@gmail.com>
2022-07-16 19:55:35 +0530

    edit and delete job functionality frontend and backend

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -20,6 +20,10 @@ import {
   GET_JOBS_BEGIN,
   GET_JOBS_SUCCESS,
   SET_EDIT_JOB,
+  DELETE_JOB_BEGIN,
+  EDIT_JOB_BEGIN,
+  EDIT_JOB_ERROR,
+  EDIT_JOB_SUCCESS,
 } from "./actions";
 
 const token = localStorage.getItem("token");
@@ -224,11 +228,39 @@ const AppProvider = ({ children }) => {
     dispatch({ type: SET_EDIT_JOB, payload: { id } });
   };
 
-  const editJob = () => {
-    console.log("edit job");
+  const editJob = async () => {
+    dispatch({ type: EDIT_JOB_BEGIN });
+    try {
+      const { editJobId, company, position, jobLocation, status, jobType } =
+        state;
+
+      await authFetch.patch(`/jobs/${editJobId}`, {
+        position,
+        company,
+        jobLocation,
+        status,
+        jobType,
+      });
+
+      dispatch({ type: EDIT_JOB_SUCCESS });
+      dispatch({ type: CLEAR_VALUES });
+    } catch (error) {
+      if (error.response.status === 401) return;
+      dispatch({
+        type: EDIT_JOB_ERROR,
+        payload: { msg: error.response.data.msg },
+      });
+    }
+    clearAlert();
   };
-  const deleteJob = (id) => {
-    console.log(`delete job : ${id}`);
+  const deleteJob = async (jobId) => {
+    dispatch({ type: DELETE_JOB_BEGIN });
+    try {
+      await authFetch.delete(`/jobs/${jobId}`);
+      getJobs();
+    } catch (error) {
+      logoutUser();
+    }
   };
 
   return (
--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -17,6 +17,10 @@ import {
   GET_JOBS_BEGIN,
   GET_JOBS_SUCCESS,
   SET_EDIT_JOB,
+  DELETE_JOB_BEGIN,
+  EDIT_JOB_BEGIN,
+  EDIT_JOB_ERROR,
+  EDIT_JOB_SUCCESS,
 } from "./actions";
 import { initialState } from "./appContext";
 
@@ -169,6 +173,31 @@ const reducer = (state, action) => {
       jobLocation,
       jobType,
       status,
+    };
+  }
+  if (action.type === DELETE_JOB_BEGIN) {
+    return { ...state, isLoading: true };
+  }
+
+  if (action.type === EDIT_JOB_BEGIN) {
+    return { ...state, isLoading: true };
+  }
+  if (action.type === EDIT_JOB_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: "success",
+      alertText: "job edited successfully",
+    };
+  }
+  if (action.type === EDIT_JOB_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: "danger",
+      alertText: action.payload.msg,
     };
   }
 
